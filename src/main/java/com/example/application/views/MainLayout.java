3cbaf0011fd68cb746b52591ce8ec242761d4758
start.vaadin.com <no-reply@vaadin.com>
2023-07-11 18:39:30 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -0,0 +1,78 @@
+package com.example.application.views;
+
+import com.example.application.views.logger.LoggerView;
+import com.example.application.views.person.PersonView;
+import com.example.application.views.projects.ProjectsView;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import org.vaadin.lineawesome.LineAwesomeIcon;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    private H2 viewTitle;
+
+    public MainLayout() {
+        setPrimarySection(Section.DRAWER);
+        addDrawerContent();
+        addHeaderContent();
+    }
+
+    private void addHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.getElement().setAttribute("aria-label", "Menu toggle");
+
+        viewTitle = new H2();
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+
+        addToNavbar(true, toggle, viewTitle);
+    }
+
+    private void addDrawerContent() {
+        H1 appName = new H1("Project activity log");
+        appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+        Header header = new Header(appName);
+
+        Scroller scroller = new Scroller(createNavigation());
+
+        addToDrawer(header, scroller, createFooter());
+    }
+
+    private SideNav createNavigation() {
+        SideNav nav = new SideNav();
+
+        nav.addItem(new SideNavItem("Logger", LoggerView.class, LineAwesomeIcon.GITTER.create()));
+        nav.addItem(new SideNavItem("Projects", ProjectsView.class, LineAwesomeIcon.PROJECT_DIAGRAM_SOLID.create()));
+        nav.addItem(new SideNavItem("Person", PersonView.class, LineAwesomeIcon.USER.create()));
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
