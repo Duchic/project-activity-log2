eb3d9fae7daf6a29bdfbedf1dbcf43966d50bca3
Tomas Duchek <neb7pxuk@duchic.anonaddy.com>
2023-07-19 21:59:48 +0200

    Some better way of PersonService getAllPerson, PersonDto builder

--- src/main/java/com/example/projectactivitylog/dto/PersonDto.java
+++ src/main/java/com/example/projectactivitylog/dto/PersonDto.java
@@ -1,8 +1,13 @@
 package com.example.projectactivitylog.dto;
 
+import com.example.projectactivitylog.entities.PersonEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@Builder
 public class PersonDto {
 
     private Integer id;
@@ -11,4 +16,11 @@ public class PersonDto {
 
     private String surname;
 
+    public static PersonDto of(PersonEntity personEntity) {
+        return PersonDto.builder()
+                .id(personEntity.getId())
+                .name(personEntity.getName())
+                .surname(personEntity.getSurname())
+                .build();
+    }
 }
--- src/main/java/com/example/projectactivitylog/servicess/PersonService.java
+++ src/main/java/com/example/projectactivitylog/servicess/PersonService.java
@@ -3,11 +3,13 @@ package com.example.projectactivitylog.servicess;
 import com.example.projectactivitylog.dto.PersonDto;
 import com.example.projectactivitylog.entities.PersonEntity;
 import com.example.projectactivitylog.repositories.PersonRepository;
+import com.vaadin.flow.router.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -19,29 +21,38 @@ public class PersonService {
     }
 
     public List<PersonDto> getAllPerson() {
-        List<PersonEntity> allPerson = personRepository.findAll();
-        List<PersonDto> dtoList = new ArrayList<>();
-
-        for (int i=0; i<allPerson.size(); i++) {
-            PersonDto personDto = new PersonDto();
-            personDto.setId(allPerson.get(i).getId());
-            personDto.setName(allPerson.get(i).getName());
-            personDto.setSurname(allPerson.get(i).getSurname());
-            dtoList.add(i, personDto);
-        }
-        return dtoList;
+//        List<PersonEntity> allPerson = personRepository.findAll();
+//        List<PersonDto> dtoList = new ArrayList<>();
+//
+//        for (int i=0; i<allPerson.size(); i++) {
+//            PersonDto personDto = new PersonDto();
+//            personDto.setId(allPerson.get(i).getId());
+//            personDto.setName(allPerson.get(i).getName());
+//            personDto.setSurname(allPerson.get(i).getSurname());
+//            dtoList.add(i, personDto);
+//        }
+//        return dtoList;
+        return personRepository.findAll().stream()
+                .map(PersonDto::of)
+                .collect(Collectors.toList());
     }
 
     public PersonDto getPersonById(int id) {
-        Optional<PersonEntity> byId = personRepository.findById(id);
-        PersonDto personDto = new PersonDto();
-        if (byId.isPresent()) {
-            personDto.setId(byId.get().getId());
-            personDto.setName(byId.get().getName());
-            personDto.setSurname(byId.get().getSurname());
-            return personDto;
-        }
-        return null;
+//        Optional<PersonEntity> byId = personRepository.findById(id);
+//        PersonDto personDto = new PersonDto();
+//        if (byId.isPresent()) {
+//            personDto.setId(byId.get().getId());
+//            personDto.setName(byId.get().getName());
+//            personDto.setSurname(byId.get().getSurname());
+//            return personDto;
+//        }
+//        return null;
+
+        return PersonDto.of(personRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException())
+                );
+
     }
 
     public PersonDto createNewPerson(PersonDto personDto) {
