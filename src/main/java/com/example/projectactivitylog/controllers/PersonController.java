f44d449bbc0da69d04daa906e04b9325b5c29852
Tomas Duchek <neb7pxuk@duchic.anonaddy.com>
2023-07-28 22:23:01 +0200

    Add controllers, can be used as RestAPI

---
+++ src/main/java/com/example/projectactivitylog/controllers/PersonController.java
@@ -0,0 +1,53 @@
+package com.example.projectactivitylog.controllers;
+
+import com.example.projectactivitylog.dto.PersonDto;
+import com.example.projectactivitylog.entities.PersonEntity;
+import com.example.projectactivitylog.servicess.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class PersonController {
+
+    private List<PersonEntity> personDB = List.of(new PersonEntity(1, "Jan", "Z Rokycan"),
+            new PersonEntity(2, "Pavel", "Test"),
+            new PersonEntity(3, "Dalsi", "Testovaci"));
+
+    private final PersonService personService;
+
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping("/person/create") //hotovo
+    public PersonDto createNewPerson(@RequestBody PersonDto personDto) {
+        return personService.createNewPerson(personDto);
+    }
+
+    @GetMapping("/person/get{id}") //hotovo
+    public PersonDto getPerson(@PathVariable int id) {
+        return personService.getPersonById(id);
+    }
+
+    @GetMapping("/person") //testovaci
+    public List<PersonEntity> getAllPerson() {
+        return personDB;
+    }
+
+    @PostMapping("/person/update/{id}")
+    public PersonDto updatePerson(@PathVariable int id, @RequestBody PersonDto personDto) {
+        return personService.updatePerson(personDto);
+    }
+
+    @DeleteMapping("/person/delete/{id}")
+    public void deletePerson(@PathVariable int id) {
+        personService.deletePerson(id);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/example/projectactivitylog/controllers/ProjectController.java
@@ -0,0 +1,57 @@
+package com.example.projectactivitylog.controllers;
+
+import com.example.projectactivitylog.dto.PersonDto;
+import com.example.projectactivitylog.dto.ProjectDto;
+import com.example.projectactivitylog.entities.ProjectEntity;
+import com.example.projectactivitylog.servicess.ProjectService;
+import com.example.projectactivitylog.util.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProjectController {
+
+    private List<ProjectEntity> projectsDB = List.of(new ProjectEntity(1, "test", "test", Constants.STATUS_ACTIVE),
+            new ProjectEntity(2, "test2", "Test2",Constants.STATUS_DONE),
+            new ProjectEntity(3, "test3", "test3", Constants.STATUS_STOPPED));
+    private final ProjectService projectService;
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping("/project/create") //hotovo
+    public ProjectDto createNewProject(@RequestBody ProjectDto projectDto) {
+        return projectService.createNewProject(projectDto);
+    }
+    @CrossOrigin
+    @GetMapping("/project/get/{id}") //hotovo
+    public ProjectDto getProject(@PathVariable int id) {
+        return projectService.getProjectById(id);
+    }
+    @CrossOrigin
+    @GetMapping("/project") //testovaci vrati list
+    public List<ProjectEntity> getAllProjects() {
+        return projectsDB;
+    }
+
+    @PostMapping("/project/update/{id}") //hotovo
+    public ProjectDto updateProject(@PathVariable int id, @RequestBody ProjectDto projectDto) {
+        return projectService.updateProject(projectDto);
+    }
+
+    @DeleteMapping("/project/delete/{id}") //hotovo
+    public void deleteProject(@PathVariable int id) {
+        projectService.deleteProject(id);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello";
+    }
+
+
+}
